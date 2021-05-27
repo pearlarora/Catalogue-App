728ad833855d8a0dd37ab9deb85f61dfc45e30dd
pearlarora <ipearlarora@gmail.com>
2021-05-27 10:41:38 +0530

    theme and routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:catalogue_app/home_page.dart';
+import 'package:catalogue_app/pages/home_page.dart';
+import 'package:catalogue_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,6 +9,29 @@ void main() {
 class CatalogueApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      // home: HomePage(),
+
+      // For App Theme: Light(default), Dark
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+          // For light theme
+          brightness: Brightness.light, //optional line
+          primarySwatch:
+              Colors.red // To tell the color pallete use primarySwatch
+          ),
+      darkTheme: ThemeData(
+          // For dark theme
+          brightness:
+              Brightness.dark // It overwrites all color palletes in dark theme
+          ),
+
+      initialRoute: "/",  // default
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
+    );
   }
 }
